65c541d42604cfea7b2e3518d29ca273758f94f9
Aleksandr Strizhevskiy <abstrizhevskiy@gmail.com>
2023-03-15 02:05:49 -0400

    Created a button displays a new word. Scramble the word.

--- challengegenerator.cpp
+++ challengegenerator.cpp
@@ -2,22 +2,31 @@
 
 
 
+//maintains a persistent connection to the database
 ChallengeGenerator::ChallengeGenerator()
 {
     db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("word.db");
 }
 
+//Generates a word challenge
 WordChallenge ChallengeGenerator::getChallenge(){
 
     if (db.open()) {
 
         QSqlQuery query;
 
+        //sets some generic string values to return incase there is no connection to the database
+        //idValue was used for debugging and will likely be removed
         QString wordResult="hello";
         QString definitionResult = "goodbye";
         QString definitionProcessed = "";
         QString idValue = "-57";
+
+
+        //Found this method for selecting a random DB entry on stack oververflow
+        //Would have prefered to generate a random ID Value and select it but for some reason some entries were not entered into DB
+        //Instead of debugging I decided not to worry about a couple of lost words and do this instead.
 
         query.exec("SELECT * FROM words WHERE id IN (SELECT id FROM words ORDER BY RANDOM() LIMIT 1)");
 
@@ -27,6 +36,8 @@ WordChallenge ChallengeGenerator::getChallenge(){
             definitionResult = query.value(3).toString();
         }
 
+        //The definitions are separated by the bar character. Here I split them and just add a newline between them
+        //May decide to split them up some other way once the UI is ready
         QStringList defSplit = definitionResult.split("|");
         for(const auto &def : defSplit)
             definitionProcessed.append(def.trimmed() + "\n");
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include <qpushbutton.h>
 
 #include <QApplication>
 #include <QFont>
@@ -10,17 +11,20 @@
 #include "challengegenerator.h"
 
 
+QString shuffle(QString word);
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
+    //Initializes the database and the WordChallenge object
     ChallengeGenerator gen;
     WordChallenge challenge = gen.getChallenge();
 
 
-
-    QLabel *label = new QLabel(challenge.getWord() + "\n" + challenge.getDefinition());
+    //Creates the label, button, alignment info, and attaches it to the window
+    QLabel *label = new QLabel("Push button to get the challenge!");
     QFont font;
     font.setWeight(QFont::Bold);
     font.setPixelSize(12);
@@ -28,8 +32,16 @@ int main(int argc, char *argv[])
     label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     label->setFont(font);
 
+    //Gets the button object defined in the ui class and attches "clicked" functionality to it (calls getChallenge())
+    QPushButton *button = w.findChild<QPushButton*>("pushButton");
+    QObject::connect(button, &QPushButton::clicked, [&]() {
+        challenge = gen.getChallenge();
+        label->setText(shuffle(challenge.getWord()) + "\n" + challenge.getWord() + "\n" + challenge.getDefinition());
+    });
+
     QVBoxLayout  *layout = new QVBoxLayout ();
     layout->addWidget(label, 0, Qt::AlignCenter);
+    layout->addWidget(button, 0, Qt::AlignCenter);
     layout->setAlignment(Qt::AlignCenter);
 
     w.setCentralWidget(new QWidget(&w));
@@ -41,4 +53,14 @@ int main(int argc, char *argv[])
     return a.exec();
 }
 
+//Shuffles the string. Will likely put this in a utilities class once I have more of the project done.
+QString shuffle(QString word){
+    QString shuffled = "";
+    QList<QString> letters = word.split("");
 
+    std::shuffle(letters.begin(), letters.end(), std::mt19937(std::random_device()()));
+        for (auto & letter : letters)
+            shuffled.append(letter);
+
+    return shuffled;
+}
